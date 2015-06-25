4d9236f8f319461962853af3e8a4dbe51d950b73
Nicolas Thal <nico.th4l@gmail.com>
2015-06-25 11:41:28 +0200

    move out WorkflowFunctionAdminBundle

---
+++ WorkflowFunctionAdminBundle/OpenOrchestraWorkflowFunctionAdminBundle.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace OpenOrchestra\WorkflowFunctionAdminBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class OpenOrchestraWorkflowFunctionAdminBundle
+ */
+class OpenOrchestraWorkflowFunctionAdminBundle extends Bundle
+{
+}
