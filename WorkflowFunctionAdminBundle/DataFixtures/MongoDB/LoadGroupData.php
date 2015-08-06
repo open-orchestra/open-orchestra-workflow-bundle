e9b7d3350a16df98183fbd0f9f92a55432513d45
romain boessel <romain.boessel@businessecision.com>
2015-08-06 18:27:40 +0200

    rename left panel to navigation panel

--- WorkflowFunctionAdminBundle/DataFixtures/MongoDB/LoadGroupData.php
+++ WorkflowFunctionAdminBundle/DataFixtures/MongoDB/LoadGroupData.php
@@ -5,7 +5,7 @@ namespace OpenOrchestra\WorkflowFunctionAdminBundle\DataFixtures\MongoDB;
 use Doctrine\Common\DataFixtures\AbstractFixture;
 use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
-use OpenOrchestra\WorkflowFunctionAdminBundle\LeftPanel\Strategies\WorkflowFunctionPanelStrategy;
+use OpenOrchestra\WorkflowFunctionAdminBundle\NavigationPanel\Strategies\WorkflowFunctionPanelStrategy;
 
 /**
  * Class LoadGroupData
