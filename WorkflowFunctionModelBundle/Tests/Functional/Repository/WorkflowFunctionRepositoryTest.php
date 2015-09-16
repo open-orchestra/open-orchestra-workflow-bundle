7ee377a7398ba7ac05113a09bcc16450e7869946
Amaury Lavieille <amaury.lavieille@businessdecision.com>
2015-09-16 14:37:31 +0200

    update test

--- WorkflowFunctionModelBundle/Tests/Functional/Repository/WorkflowFunctionRepositoryTest.php
+++ WorkflowFunctionModelBundle/Tests/Functional/Repository/WorkflowFunctionRepositoryTest.php
@@ -131,7 +131,7 @@ class WorkflowFunctionRepositoryTest extends KernelTestCase
     protected function getDescriptionColumnEntity()
     {
         return array(
-            'name' => new Search(array('key' => 'name', 'field' => 'name', 'type' => 'string')),
+            'name' => array('key' => 'name', 'field' => 'name', 'type' => 'string'),
         );
     }
 
