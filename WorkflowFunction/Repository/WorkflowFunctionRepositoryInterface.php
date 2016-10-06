dcb00d697ed11e8dedf4d7baa395edac81e0fe41
Nicolas ANNE <nicolas.anne@businessdecision.com>
2016-10-06 10:38:22 +0200

    generalize interface

--- WorkflowFunction/Repository/WorkflowFunctionRepositoryInterface.php
+++ WorkflowFunction/Repository/WorkflowFunctionRepositoryInterface.php
@@ -7,34 +7,13 @@ use OpenOrchestra\ModelInterface\Model\RoleInterface;
 use OpenOrchestra\Pagination\Configuration\FinderConfiguration;
 use OpenOrchestra\Pagination\Configuration\PaginateFinderConfiguration;
 use OpenOrchestra\ModelInterface\Repository\RoleableElementRepositoryInterface;
+use OpenOrchestra\Pagination\Configuration\PaginationRepositoryInterface;
 
 /**
  * Interface WorkflowFunctionRepositoryInterface
  */
-interface WorkflowFunctionRepositoryInterface extends RoleableElementRepositoryInterface
+interface WorkflowFunctionRepositoryInterface extends RoleableElementRepositoryInterface, PaginationRepositoryInterface
 {
-    /**
-     * @param PaginateFinderConfiguration $configuration
-     *
-     * @return mixed
-     * @throws \Doctrine\ODM\MongoDB\MongoDBException
-     */
-    public function findForPaginate(PaginateFinderConfiguration $configuration);
-
-    /**
-     * @param FinderConfiguration $configuration
-     *
-     * @return int
-     */
-    public function count(FinderConfiguration $configuration);
-
-    /**
-     * @param FinderConfiguration $configuration
-     *
-     * @return mixed
-     */
-    public function countWithFilter(FinderConfiguration $configuration);
-
     /**
      * @return Collection
      */
